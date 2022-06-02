90d9e65bb61214eaad698bdf11e382a8c2dce3e2
Diego Bryan <thediegobryan@gmail.com>
2022-06-02 17:51:26 -0400

    Finished

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let profilePage = document.querySelector("#profilePage")
 let gitHubUrl = 'https://api.github.com/users/thediegobryan'
 let gitHubRepoUrl = 'https://api.github.com/users/thediegobryan/repos'
 
+//Function for gathering header data
 function buildProfile(profileData){
     // console.log(profileData)
     let profileBox = document.createElement("div");
@@ -44,6 +45,31 @@ function buildProfile(profileData){
     profileBox.appendChild(profileUsername);
 }
 
+//Function for RepoList
+function buildRepoList(repoData){
+    console.log(repoData.html_url)
+    let ulElemenent = document.createElement("ul")
+    ulElemenent.classList.add("list-group")
+    profilePage.appendChild(ulElemenent);
+    let liTitle = document.createElement("li")
+    liTitle.classList.add("list-group-item", "text-bg-light", "p-3")
+    liTitle.innerText = "GitHub Repos";
+    ulElemenent.appendChild(liTitle)
+    for(repo of repoData){
+        console.log(repo.html_url)
+        let liElement = document.createElement("li")
+        liElement.classList.add("list-group-item")
+        let anchorRepoUrl = document.createElement("a");
+        anchorRepoUrl.href = repo.html_url;
+        anchorRepoUrl.target = 'blank'
+        anchorRepoUrl.innerText = repo.name;
+        liElement.appendChild(document.createTextNode("📁 "))
+        liElement.appendChild(anchorRepoUrl);
+        ulElemenent.appendChild(liElement);
+
+    }
+}
+
 fetch(gitHubUrl, {
     method: 'GET',
     headers: {'Content-Type': 'application/json'}
@@ -72,23 +98,3 @@ fetch(gitHubRepoUrl, {
     // console.log(data[1].name)
     buildRepoList(data);
 })
-
-function buildRepoList(repoData){
-    console.log(repoData.html_url)
-    let ulElemenent = document.createElement("ul")
-    ulElemenent.classList.add("list-group")
-    profilePage.appendChild(ulElemenent);
-    for(repo of repoData){
-        console.log(repo.html_url)
-        let liElement = document.createElement("li")
-        liElement.classList.add("list-group-item")
-        let anchorRepoUrl = document.createElement("a");
-        anchorRepoUrl.href = repo.html_url;
-        anchorRepoUrl.target = 'blank'
-        anchorRepoUrl.innerText = repo.name;
-        liElement.appendChild(document.createTextNode("📁 "))
-        liElement.appendChild(anchorRepoUrl);
-        ulElemenent.appendChild(liElement);
-
-    }
-}
